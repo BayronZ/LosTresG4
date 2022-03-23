e91e9ceaa63678d78f71ea842a666dcf149a216f
BayronZ <bayron.zavalag@gmail.com>
2022-03-22 21:04:24 -0300

    Finished: juego.rb

---
+++ lostres/juego.rb
@@ -0,0 +1,54 @@
+# 0 es piedra
+# 1 es papel
+# 2 es tijera
+
+
+
+#computador elige al azar un número
+
+pc = rand(0..2)
+
+#usuario ingresauna opción con ARGV entre piedra papel o tijera
+
+user = ARGV[0].downcase
+
+
+
+if user == "piedra"
+    if pc == 0
+        puts "Computador juega piedra"
+        puts "Empataste"
+    elsif pc == 1
+        puts "Computador juega papel"
+        puts "perdiste"
+    else
+        puts "Computador juega tijera"
+        puts "Ganaste!"
+    end
+elsif user == "papel"
+    if pc == 0
+        puts "Computador juega piedra"
+        puts "Ganaste!"
+    elsif pc == 1
+        puts "Computador juega papel"
+        puts "Empataste"
+    else
+        puts "Computador juega tijera"
+        puts "Perdiste!"
+    end
+
+elsif user == "tijera"
+    if pc == 0
+        puts "Computador juega piedra"
+        puts "Perdiste"
+    elsif pc == 1
+        puts "Computador juega papel"
+        puts "Ganaste!"
+    else
+        puts "Computador juega tijera"
+        puts "Empataste"
+    end
+else
+    puts "Argumento invalido: Debe ser piedra, papel o tijera"
+
+end
